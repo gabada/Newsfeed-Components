b4787cd48b95ce697976392a823c479b390beaa7
Gill Abada <gill@callifi.com>
2019-01-30 16:37:12 -0500

    add toggle for articles.

--- Article/Article.js
+++ Article/Article.js
@@ -14,7 +14,8 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    console.log(this.domElement);
+    this.domElement.classList.toggle('article-open');
   }
 }
 
